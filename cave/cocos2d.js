49240eb0de029c4ef8b047862a5d1697cefa616c
Toru Matsuoka <lettas0726@gmail.com>
2014-01-19 20:04:49 +0900

    Off debug mode

--- cave/cocos2d.js
+++ cave/cocos2d.js
@@ -27,10 +27,10 @@
 (function () {
     var d = document;
     var c = {
-        COCOS2D_DEBUG:2, //0 to turn debug off, 1 for basic debug, and 2 for full debug
+        COCOS2D_DEBUG:0, //0 to turn debug off, 1 for basic debug, and 2 for full debug
         box2d:false,
         chipmunk:false,
-        showFPS:true,
+        showFPS:false,
         loadExtension:false,
         frameRate:60,
         renderMode:0,       //Choose of RenderMode: 0(default), 1(Canvas only), 2(WebGL only)
